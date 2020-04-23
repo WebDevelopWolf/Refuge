61c886fde0c8b740890227e62016e81b1f353ea7
Liane <webdevelopwolf@gmail.com>
2020-04-23 03:55:14 +0100

    API & UX Basic Settings Complete

--- Refuge-UX/src/app/home/home.component.ts
+++ Refuge-UX/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  story: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getStory();
+  }
+
+  getStory() {
+    this.http.get('http://localhost:5000/api/stories/1').subscribe(response => {
+      this.story = response;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
---
+++ Refuge.Engine/Controllers/StoriesController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Refuge.Engine.Data;
+
+namespace Refuge.Engine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public StoriesController(DataContext context)
+        {
+            _context = context;
+
+        }
+        // GET - List of Availible Stories
+        [HttpGet]
+        public async Task<IActionResult> GetStories()
+        {
+            var stories = await _context.Stories.ToListAsync();
+            return Ok(stories);
+        }
+
+        // GET - Single Story
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStory(int id)
+        {
+            var story = await _context.Stories.FirstOrDefaultAsync(s => s.Id == id);
+            return Ok(story);
+        }
+    }
+}
+
+
+
